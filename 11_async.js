6f294342f5ea753c0772a239f7f5e83b5e3ee446
Baptiste <bc@scrinfo.net>
2025-04-07 14:48:31 +0200

    reponse 11

--- 11_async.js
+++ 11_async.js
@@ -1,4 +1,4 @@
-//const {sleep} = require("../exercices/10_promise");
+const {sleep} = require("../ExerciceJS/10_promise");
 
 /**
  * Créez une fonction synchrone qui attend 2 seconde puis execute le callback passé en paramètre
@@ -12,6 +12,7 @@
  * 
  */
 const usingThen = (cb) => {
+    sleep().then(() => {cb()});
 }
 
 /**
@@ -25,8 +26,9 @@ const usingThen = (cb) => {
  *   - ne pas utiliser .then
  */
 
-const usingAwait = (cb) => {
-
+const usingAwait = async (cb) => {
+    await sleep();
+    cb();
 }
 
 /**
@@ -42,11 +44,8 @@ const usingAwait = (cb) => {
  */
 
 //décommentez la ligne suivante une fois le package installé
-//const axios = require("axios");
+const axios = require("axios");
 
-const apiResponse = async (url) => {
-
-}
-
+const apiResponse = async (url) => (await axios.get(url)).data
 
 module.exports = {usingThen, usingAwait, apiResponse};
